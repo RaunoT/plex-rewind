e7daeb89880bddb07d30d6f9e21569ac1667f900
RaunoTM <rauno.tegelmann@monese.com>
2022-12-10 09:09:52 +0200

    Allow setting Tautulli external url

--- utils/fetchStats.js
+++ utils/fetchStats.js
@@ -1,5 +1,7 @@
 async function fetchStats(query) {
-  const apiUrl = `http://127.0.0.1:${process.env.TAUTULLI_PORT}/api/v2?apikey=${process.env.TAUTULLI_API_KEY}`
+  const apiUrl = `${process.env.TAUTULLI_URL}${
+    process.env.TAUTULLI_PORT ? ':' + process.env.TAUTULLI_PORT : ''
+  }/api/v2?apikey=${process.env.TAUTULLI_API_KEY}`
   const response = await fetch(`${apiUrl}&cmd=${query}`)
   const result = await response.json()
 
