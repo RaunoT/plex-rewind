fd78e7e3dd00c4afbc9f45ed4c10c0eb2abc3242
RaunoT <raunoot@gmail.com>
2022-12-26 14:05:24 +0200

    Add some caching to improve performance

--- utils/fetchFromTautulli.js
+++ utils/fetchFromTautulli.js
@@ -3,7 +3,9 @@ async function fetchFromTautulli(query, params) {
   const apiUrl = `${process.env.NEXT_PUBLIC_TAUTULLI_URL}/api/v2?apikey=${process.env.TAUTULLI_API_KEY}`
   const paramsStr = params ? '&' + new URLSearchParams(params).toString() : ''
 
-  const res = await fetch(`${apiUrl}&cmd=${query}${paramsStr}`)
+  const res = await fetch(`${apiUrl}&cmd=${query}${paramsStr}`, {
+    next: { revalidate: 300 },
+  })
   const data = await res.json()
 
   return data
