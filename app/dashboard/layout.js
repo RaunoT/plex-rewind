1dbef4bf590b72cdd941a4d3ec23ac427ffee3c2
RaunoT <raunoot@gmail.com>
2022-12-29 17:36:20 +0200

    Scroll to top on dashboard page change

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,7 +1,17 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
 import Card from '../../ui/Card'
 import PageTitle from '../../ui/PageTitle'
 
 export default function DashboardLayout({ children }) {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <div className="w-full max-w-2xl xl:max-w-5xl">
       <PageTitle title="Dashboard" subtitle="last 30 days" />
