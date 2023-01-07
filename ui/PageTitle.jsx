20abb43ec1a78509abe0b99e4fb8519d5c8088a3
RaunoT <raunoot@gmail.com>
2023-01-07 16:28:28 +0200

    Added timeframe selection buttons to Dashboard

--- ui/PageTitle.jsx
+++ ui/PageTitle.jsx
@@ -3,23 +3,34 @@ import clsx from 'clsx'
 import Link from 'next/link'
 import React from 'react'
 
-export default function PageTitle({ title, subtitle }) {
+export default function PageTitle({ title }) {
   return (
-    <div className="relative w-full mb-4 text-center uppercase">
-      <Link
-        href="/"
-        className={clsx('w-5 block ml-5', {
-          'absolute my-auto -translate-y-1/2 left-0 top-1/2': title,
-        })}
-      >
-        <ArrowLeftIcon />
-      </Link>
-      {title && <h1 className="text-xl font-bold sm:text-2xl">{title}</h1>}
-      {subtitle && (
-        <span className="block text-xs font-medium text-teal-300 sm:text-sm">
-          {subtitle}
-        </span>
-      )}
+    <div className="w-full mb-2">
+      <div className="relative text-center uppercase">
+        <Link
+          href="/"
+          className={clsx('w-5 block ml-5', {
+            'absolute my-auto -translate-y-1/2 left-0 top-1/2': title,
+          })}
+        >
+          <ArrowLeftIcon />
+        </Link>
+        {title && <h1 className="text-xl font-bold sm:text-2xl">{title}</h1>}
+      </div>
+      <ul className="flex items-center text-teal-300 font-medium gap-4 mt-3 justify-center text-xs sm:text-base">
+        <li>
+          <button className="uppercase">30 days</button>
+        </li>
+        <li>
+          <button className="uppercase text-white">3 months</button>
+        </li>
+        <li>
+          <button className="uppercase text-white">This year</button>
+        </li>
+        <li>
+          <button className="uppercase text-white">All time</button>
+        </li>
+      </ul>
     </div>
   )
 }
