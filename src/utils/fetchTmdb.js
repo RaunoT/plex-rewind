f688693a823739cb5544fcc75f5fb7a09cc7b720
poleloigu <oleloigujr@gmail.com>
2023-06-08 11:43:26 +0300

    good refactor

--- src/utils/fetchTmdb.js
+++
@@ -1,9 +0,0 @@
-export default async function fetchTmdb(endpoint, params) {
-  // TODO: Replace with IMDB for movies to have consistency with Plex
-  const query = params ? '&' + new URLSearchParams(params).toString() : ''
-  const apiUrl = `https://api.themoviedb.org/3/${endpoint}?api_key=${process.env.TMDB_API_KEY}${query}`
-  const res = await fetch(apiUrl)
-  const data = await res.json()
-
-  return data
-}
