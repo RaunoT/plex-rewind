255aebc2a1c3d9c630d4e59f5bc9554484feaea8
RaunoT <raunoot@gmail.com>
2023-07-29 20:12:33 +0300

    Add dockerfile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 /** https://nextjs.org/docs/api-reference/next.config.js/introduction */
 module.exports = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
@@ -9,5 +10,39 @@ module.exports = {
         hostname: 'tautulli.rauno.eu',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=(), payment=()',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' data:; connect-src 'self';",
+          },
+        ],
+      },
+    ]
   },
 }
