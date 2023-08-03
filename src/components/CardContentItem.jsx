fe58fba5a7c8a8781d02a3692eae83a2a88bdad2
Rauno Tegelmann <raunoot@gmail.com>
2023-08-03 17:57:48 +0300

    Fix medals alignment

--- src/components/CardContentItem.jsx
+++ src/components/CardContentItem.jsx
@@ -86,43 +86,41 @@ export default function CardContentItem({
       </div>
       <div>
         <h3 className='mb-2 sm:text-xl'>
-          <div className='inline-flex items-center gap-1 mr-2'>
+          <div className='inline-flex items-baseline gap-1 mr-1.5'>
             <span className='font-bold text-black'>#{i + 1} </span>
             {i < 3 && (
               <svg width='16px' viewBox='0 0 300.439 300.439'>
-                <g>
-                  <path
-                    className='fill-orange-700'
-                    d='M276.967,0h-84.498L70.415,178.385h84.498L276.967,0z'
-                  />
-                  <path
-                    className='fill-orange-600'
-                    d='M23.472,0h84.498l122.053,178.385h-84.498L23.472,0z'
-                  />
-                  <path
-                    className={
-                      i === 0
-                        ? 'fill-yellow-300'
-                        : i === 1
-                        ? 'fill-gray-300'
-                        : 'fill-yellow-700'
-                    }
-                    d='M154.914,93.887c57.271,0,103.276,46.005,103.276,103.276s-46.005,103.276-103.276,103.276
+                <path
+                  className='fill-orange-700'
+                  d='M276.967,0h-84.498L70.415,178.385h84.498L276.967,0z'
+                />
+                <path
+                  className='fill-orange-600'
+                  d='M23.472,0h84.498l122.053,178.385h-84.498L23.472,0z'
+                />
+                <path
+                  className={
+                    i === 0
+                      ? 'fill-yellow-300'
+                      : i === 1
+                      ? 'fill-gray-300'
+                      : 'fill-yellow-700'
+                  }
+                  d='M154.914,93.887c57.271,0,103.276,46.005,103.276,103.276s-46.005,103.276-103.276,103.276
 		                  S51.638,254.434,51.638,197.163S97.643,93.887,154.914,93.887z'
-                  />
-                  <path
-                    className={
-                      i === 0
-                        ? 'fill-yellow-500'
-                        : i === 1
-                        ? 'fill-gray-400'
-                        : 'fill-yellow-800'
-                    }
-                    d='M154.914,122.053c-41.31,0-75.11,33.799-75.11,75.11s33.799,75.11,75.11,75.11
+                />
+                <path
+                  className={
+                    i === 0
+                      ? 'fill-yellow-500'
+                      : i === 1
+                      ? 'fill-gray-400'
+                      : 'fill-yellow-800'
+                  }
+                  d='M154.914,122.053c-41.31,0-75.11,33.799-75.11,75.11s33.799,75.11,75.11,75.11
 		                  s75.11-33.799,75.11-75.11S196.224,122.053,154.914,122.053z M154.914,253.495c-30.983,0-56.332-25.35-56.332-56.332
 		                  s25.35-56.332,56.332-56.332s56.332,25.35,56.332,56.332S185.896,253.495,154.914,253.495z'
-                  />
-                </g>
+                />
               </svg>
             )}
           </div>
