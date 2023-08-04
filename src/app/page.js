8679e3c9417eba75740475ba555d6732bbc3dbb9
RaunoT <raunoot@gmail.com>
2023-08-04 11:12:08 +0300

    Fix conditional

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export const metadata = {
 }
 
 export default function Page() {
-  const isRewindDisabled = process.env.NEXT_PUBLIC_IS_REWIND_DISABLED
+  const isRewindDisabled = process.env.NEXT_PUBLIC_IS_REWIND_DISABLED === 'true'
 
   return (
     <div className='text-center'>
