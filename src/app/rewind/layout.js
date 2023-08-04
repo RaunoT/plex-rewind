b13e9beeb717ef183deb0f1db2fa76f0ca9ae756
RaunoT <raunoot@gmail.com>
2023-08-04 11:14:09 +0300

    Addition fo previous commit

--- src/app/rewind/layout.js
+++ src/app/rewind/layout.js
@@ -3,7 +3,7 @@ import PageTitle from '@/components/PageTitle'
 import { notFound } from 'next/navigation'
 
 export default function RewindLayout({ children }) {
-  return process.env.NEXT_PUBLIC_IS_REWIND_DISABLED ? (
+  return process.env.NEXT_PUBLIC_IS_REWIND_DISABLED === 'true' ? (
     notFound()
   ) : (
     <div className='w-full max-w-2xl'>
