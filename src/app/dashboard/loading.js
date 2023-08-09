e7f5fb860844d21f2d198108dc60c880122ae679
RaunoT <raunoot@gmail.com>
2023-08-09 15:33:23 +0300

    Fix loader on mobile

--- src/app/dashboard/loading.js
+++ src/app/dashboard/loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
       <div className='w-1/3 mb-2 sm:w-1/4 skeleton sm:skeleton--large'></div>
       <div className='w-1/5 skeleton animation-delay-200'></div>
 
-      <ul className='grid mt-4 xl:grid-cols-2 xl:grid-rows-3 sm:mt-6 xl:gap-x-8 gap-y-3 sm:gap-y-5'>
+      <ul className='grid pb-10 mt-4 xl:grid-cols-2 xl:grid-rows-3 sm:mt-6 xl:gap-x-8 gap-y-3 sm:gap-y-5'>
         <ListItem />
         <ListItem />
         <ListItem />
@@ -18,7 +18,7 @@ export default function Loading() {
 
 function ListItem() {
   return (
-    <li className='flex items-center gap-3'>
+    <li className='flex items-center gap-3 last:hidden xl:last:flex'>
       <div className='flex-shrink-0 w-20 rounded-none aspect-[2/3] skeleton'></div>
       <div className='w-full'>
         <h3 className='w-2/3 mb-2 sm:w-1/3 skeleton'></h3>
