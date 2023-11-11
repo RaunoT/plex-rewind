3242df43280cf9aa3aa7a176a6976b77271b9f50
RaunoT <raunoot@gmail.com>
2023-11-11 16:17:37 +0200

    Update packages

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ export default function Card({ children, className }: Props) {
     <div className='relative'>
       <article
         className={clsx(
-          'px-6 sm:px-8 pt-8 pb-3 sm:pb-5 rounded-3xl w-full flex flex-col bg-gradient',
+          'px-6 sm:px-8 pt-8 pb-3 rounded-3xl w-full flex flex-col bg-gradient sm:pb-5',
           className
         )}
       >
