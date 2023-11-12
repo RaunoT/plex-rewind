fe98f2a4f1794039c9a9a3367371e369f1e77861
RaunoT <raunoot@gmail.com>
2023-11-12 13:26:46 +0200

    Fix: Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -39,7 +39,7 @@ module.exports = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' data:; connect-src 'self';",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' data:; connect-src 'self' *.google-analytics.com;",
           },
         ],
       },
