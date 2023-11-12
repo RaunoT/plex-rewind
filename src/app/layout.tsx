3718e8abf80df994b885388f53bb28c9cad998a7
RaunoT <raunoot@gmail.com>
2023-11-12 09:21:57 +0200

    GH-12: Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 import '@/styles/globals.css'
 import { Viewport } from 'next'
 import Wrapper from './wrapper'
@@ -14,6 +15,10 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang='en'>
       <body className='min-height-screen bg-gradient-to-br from-indigo-900 via-neutral-800 to-neutral-900 text-white'>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
+          <GoogleAnalytics id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
+        )}
+
         <Wrapper>{children}</Wrapper>
       </body>
     </html>
