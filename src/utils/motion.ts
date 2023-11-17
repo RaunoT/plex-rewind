1d23031fb385c8e7934fc614099ca5c00d96ccaa
RaunoT <raunoot@gmail.com>
2023-11-17 09:38:14 +0200

    GH-6: Remove unneeded packages

--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -20,7 +20,7 @@ export const fadeIn = {
 
 export const animateCardText = {
   hidden: {
-    opacity: 1, //TODO: opacity: 0 broken on initial render
+    opacity: 1, // TODO: opacity: 0 broken on initial render
     y: 50,
   },
   show: {
