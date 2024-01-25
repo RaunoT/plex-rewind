c79de2bd557843319ed927cc0eef3b9cc048f2b6
Rauno Tegelmann <raunoot@gmail.com>
2024-01-25 13:06:09 +0200

    fix: settings path

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import { Settings } from '@/types'
 import settingsFile from './settings.json'
 
-export const settingsPath = './src/config/settings.json'
+export const settingsPath = '/src/config/settings.json'
 
 export const settings: Settings = settingsFile
