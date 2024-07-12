220d712b6a7c024dc18f50aacc7de56e18296af5
RaunoT <raunoot@gmail.com>
2024-07-12 21:20:55 +0300

    fix: useSearchParams usage

--- src/app/dashboard/_components/PeriodSelect.tsx
+++ src/app/dashboard/_components/PeriodSelect.tsx
@@ -1,52 +1,10 @@
-'use client'
-
-import Link from 'next/link'
-import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { Suspense } from 'react'
+import PeriodSelectContent from './PeriodSelectContent'
 
 export default function PeriodSelect() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const period = searchParams.get('period')
-
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-
   return (
-    <ul className='nav'>
-      <li>
-        <Link
-          href={`${pathname}?period=7days`}
-          className='nav-link'
-          aria-selected={period === '7days'}
-        >
-          7 days
-        </Link>
-      </li>
-      <li>
-        <Link href={`${pathname}`} className='nav-link' aria-selected={!period}>
-          30 days
-        </Link>
-      </li>
-      <li>
-        <Link
-          href={`${pathname}?period=pastYear`}
-          className='nav-link'
-          aria-selected={period === 'pastYear'}
-        >
-          Past year
-        </Link>
-      </li>
-      <li>
-        <Link
-          href={`${pathname}?period=allTime`}
-          className='nav-link'
-          aria-selected={period === 'allTime'}
-        >
-          All time
-        </Link>
-      </li>
-    </ul>
+    <Suspense>
+      <PeriodSelectContent />
+    </Suspense>
   )
 }
