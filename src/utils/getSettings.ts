a6fb1b11ec5b92fcd4bc7311abbc59fb325d2ce2
Rauno Tegelmann <raunoot@gmail.com>
2024-07-24 11:12:26 +0300

    revert: 841c39272ff04919e71ebab4c9f4c9916cf2931b

--- src/utils/getSettings.ts
+++ src/utils/getSettings.ts
@@ -2,27 +2,10 @@
 
 import { Settings } from '@/types'
 import { promises as fs } from 'fs'
-import path from 'path'
 import { DEFAULT_SETTINGS, SETTINGS_PATH } from './constants'
-
-async function ensureDirectoryExistence(filePath: string) {
-  const dirname = path.dirname(filePath)
-
-  try {
-    await fs.access(dirname)
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    if (error.code === 'ENOENT') {
-      await fs.mkdir(dirname, { recursive: true })
-    } else {
-      throw new Error('Unable to read config directory!')
-    }
-  }
-}
 
 export default async function getSettings(): Promise<Settings> {
   try {
-    await ensureDirectoryExistence(SETTINGS_PATH)
     try {
       // Attempt to read the file
       const file = await fs.readFile(SETTINGS_PATH, 'utf8')
@@ -30,6 +13,7 @@ export default async function getSettings(): Promise<Settings> {
       return JSON.parse(file)
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
+      // If reading fails because the file does not exist, create the file with default settings
       if (error.code === 'ENOENT') {
         console.warn('Settings file not found. Creating a new one.')
 
@@ -41,10 +25,12 @@ export default async function getSettings(): Promise<Settings> {
 
         return DEFAULT_SETTINGS
       } else {
-        throw new Error('Could not read settings file!')
+        console.error('Error reading settings file:', error)
+        throw new Error('Could not read settings file')
       }
     }
   } catch (error) {
-    throw new Error('Unexpected error handling settings file!')
+    console.error('Failed to handle settings file:', error)
+    throw new Error('Unexpected error handling settings file')
   }
 }
