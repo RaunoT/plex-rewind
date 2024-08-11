23498d92a7f9088cdb7e5fee7f873c7e9272320a
Dario Portmann <daerup@gmx.ch>
2024-08-11 18:18:13 +0200

    fix: fixed library listing with outside access option

--- src/utils/fetchTautulli.ts
+++ src/utils/fetchTautulli.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { authOptions } from '@/lib/auth'
+import { Settings } from '@/types/settings'
 import { TautulliLibrary, TautulliUser } from '@/types/tautulli'
 import { kebabCase } from 'lodash'
 import { getServerSession } from 'next-auth'
@@ -92,31 +93,27 @@ export async function getLibraries(
   excludeInactive = true,
 ): Promise<TautulliLibrary[]> {
   const settings = getSettings()
-  const activeLibraries = settings.general.activeLibraries
-  let libraries = (await fetchTautulli<TautulliLibrary[]>('get_libraries'))
+  const libraries = (await fetchTautulli<TautulliLibrary[]>('get_libraries'))
     ?.response?.data
   if (libraries == null) {
     console.warn('[TAUTULLI] - No libraries found!')
     return []
   }
 
-  const session = await getServerSession(authOptions)
-  const userId = session?.user.id
-
-  const sharedLibrariesResponse = await fetchTautulli<TautulliUser>(
-    'get_user',
-    { user_id: userId! },
+  const activeLibraries = filterByActiveLibraries(
+    excludeInactive,
+    settings,
+    libraries,
   )
-  if (!sharedLibrariesResponse) {
-    console.warn(`[TAUTULLI] - No shared libraries for user ${userId} found!`)
-    return []
-  }
+  return await filterBySharedLibraries(settings, activeLibraries)
+}
 
-  libraries = libraries.filter((library) =>
-    sharedLibrariesResponse.response.data.shared_libraries.includes(
-      library.section_id,
-    ),
-  )
+function filterByActiveLibraries(
+  excludeInactive: boolean,
+  settings: Settings,
+  libraries: TautulliLibrary[],
+): TautulliLibrary[] {
+  const activeLibraries = settings.general.activeLibraries
 
   if (excludeInactive) {
     libraries = libraries.filter((library) =>
@@ -124,7 +121,43 @@ export async function getLibraries(
     )
   }
 
-  return libraries
+  return excludeInactive
+    ? libraries.filter((library) =>
+        activeLibraries.includes(kebabCase(library.section_name)),
+      )
+    : libraries
+}
+
+async function filterBySharedLibraries(
+  settings: Settings,
+  libraries: TautulliLibrary[],
+): Promise<TautulliLibrary[]> {
+  if (settings.general.isOutsideAccess) {
+    return libraries
+  }
+
+  const session = await getServerSession(authOptions)
+  const userId = session?.user.id
+
+  if (!userId) {
+    console.error('[TAUTULLI] - No user ID found!')
+    return []
+  }
+
+  const userDataResponse = await fetchTautulli<TautulliUser>('get_user', {
+    user_id: userId,
+  })
+
+  if (!userDataResponse) {
+    console.warn(`[TAUTULLI] - Could not fetch user data for user ID ${userId}`)
+    return []
+  }
+
+  return libraries.filter((library) =>
+    userDataResponse.response.data.shared_libraries.includes(
+      library.section_id,
+    ),
+  )
 }
 
 export async function getLibrariesByType(
