a343a6a005f8111ca886db4dbbd0a77950e0fec6
Rauno Tegelmann <raunoot@gmail.com>
2024-08-21 09:21:23 +0300

    revert: reset settings saved message after 3s
    This reverts commit e983713a8440e2572a61db4b47fe10af67c5231a.

--- src/app/settings/_components/SettingsForm.tsx
+++ src/app/settings/_components/SettingsForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Settings } from '@/types/settings'
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode } from 'react'
 import { useFormState } from 'react-dom'
 import SettingsSaveButton from './SettingsSaveButton'
 
@@ -26,21 +26,6 @@ export default function SettingsForm({
     fields: settings,
   }
   const [formState, formAction] = useFormState(action, initialState)
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-
-  useEffect(() => {
-    if (formState.status === 'success') {
-      setShowSuccessMessage(true)
-
-      const timer = setTimeout(() => {
-        setShowSuccessMessage(false)
-      }, 3000)
-
-      return () => clearTimeout(timer)
-    } else {
-      setShowSuccessMessage(false)
-    }
-  }, [formState.status])
 
   return (
     <form className='glass-sheet pb-6' action={formAction}>
@@ -48,19 +33,18 @@ export default function SettingsForm({
         {children}
         {!hideSubmit && (
           <div className='flex flex-col items-center justify-end gap-2 sm:flex-row sm:gap-4'>
-            {(showSuccessMessage || formState.status === 'error') && (
-              <p
-                aria-live='polite'
-                role='status'
-                className={
-                  formState.status === 'success'
-                    ? 'text-green-600'
-                    : 'text-red-500'
-                }
-              >
-                {formState.message}
-              </p>
-            )}
+            <p
+              aria-live='polite'
+              role='status'
+              className={
+                formState.status === 'success'
+                  ? 'text-green-600'
+                  : 'text-red-500'
+              }
+            >
+              {formState.message}
+            </p>
+
             <SettingsSaveButton />
           </div>
         )}
