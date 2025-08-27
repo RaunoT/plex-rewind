625ef029e55c9fd9366f379410348ea6d101b034
Rauno Tegelmann <raunoot@gmail.com>
2025-08-27 14:58:08 +0300

    fix: missing check

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -2,7 +2,7 @@ import githubSvg from '@/assets/github.svg'
 import { authOptions } from '@/lib/auth'
 import getSettings from '@/utils/getSettings'
 import getVersion from '@/utils/getVersion'
-import { checkRequiredSettings } from '@/utils/helpers'
+import { checkRequiredSettings, isInitialSetup } from '@/utils/helpers'
 import { CurrencyEuroIcon, HeartIcon } from '@heroicons/react/24/outline'
 import { getServerSession } from 'next-auth'
 import { getTranslations } from 'next-intl/server'
@@ -22,12 +22,10 @@ export default async function SettingsLayout({ children }: Props) {
   const version = await getVersion()
   const missingSetting = checkRequiredSettings(settings)
   const t = await getTranslations('Settings')
+  const initialSetupMode = isInitialSetup(settings)
 
-  if (
-    !session?.user.isAdmin &&
-    missingSetting !== 'connection.tautulliUrl' &&
-    missingSetting !== 'connection.tautulliApiKey'
-  ) {
+  // Require admin for settings except during initial setup
+  if (!initialSetupMode && !session?.user?.isAdmin) {
     return notFound()
   }
 
